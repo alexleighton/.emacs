d93b991b3bde913dfb667eb9df4d4b31f65e208a
Alex Leighton <axlelonghorn@gmail.com>
2018-12-16 19:48:47 -0800

    Modernize show-paren-mode usage.

--- appearance/init-appearance.el
+++ appearance/init-appearance.el
@@ -81,7 +81,9 @@
 (setq show-paren-delay 0)
 (show-paren-mode t)
 (setq show-paren-style 'mixed)
-(set-face-background 'show-paren-match-face "#aaaaaa")
+(require 'paren)
+(set-face-background 'show-paren-match "#aaaaaa")
+(set-face-attribute 'show-paren-match nil :weight 'extra-bold)
 
 
 
